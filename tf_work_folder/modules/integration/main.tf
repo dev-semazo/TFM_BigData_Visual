02f075a3da7b694affdd88144b304fdccb1660f1
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 16:38:50 -0500

    ADD: Integration Module

---
+++ tf_work_folder/modules/integration/main.tf
@@ -0,0 +1,49 @@
+resource "aws_apigatewayv2_api" "http_api" {
+    name          = "${var.project_name}api"
+    protocol_type = "HTTP"
+    target = var.lambda_arn
+}
+
+resource "aws_apigatewayv2_integration" "lambda_integration" {
+    api_id                 = aws_apigatewayv2_api.http_api.id
+    integration_type       = "AWS_PROXY"
+    integration_uri        = var.lambda_arn
+    integration_method     = "POST"
+    payload_format_version = "2.0"
+    timeout_milliseconds = 29000 # Máximo 29 segundos para HTTP APIs
+}
+
+resource "aws_apigatewayv2_authorizer" "cognito_authorizer_v2" {
+  api_id           = aws_apigatewayv2_api.http_api.id
+  name             = "${var.project_name}-cognito-authorizer"
+  authorizer_type  = "JWT"
+  identity_sources = ["$request.header.Authorization"] 
+
+  # Configuración del proveedor JWT
+  jwt_configuration {
+    audience = ["${var.cognito_user_pool_id}"] 
+    issuer   = "https://cognito-idp.${var.aws_region}.amazonaws.com/${var.cognito_user_pool_id}"
+  }
+}
+
+resource "aws_apigatewayv2_route" "post_data_route" {
+  api_id    = aws_apigatewayv2_api.main_http_api.id
+  route_key = "POST /data" # Combina método HTTP y path
+  target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
+  authorizer_id = aws_apigatewayv2_authorizer.cognito_authorizer_v2.id
+  authorization_type = "JWT" # Usa el tipo JWT para el autorizador Cognito
+}
+
+resource "aws_apigatewayv2_stage" "api_stage_v2" {
+  api_id      = aws_apigatewayv2_api.main_http_api.id
+  name        = "prod"
+  auto_deploy = true # Despliega automáticamente los cambios en las rutas/integraciones
+}
+
+resource "aws_lambda_permission" "allow_apigatewayv2_to_invoke_lambda" {
+  statement_id  = "AllowAPIGatewayV2Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = var.lambda_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.http_api.execution_arn}/*"
+}
---
+++ tf_work_folder/modules/integration/variables.tf
@@ -0,0 +1,31 @@
+variable "aws_region" {
+  description = "Región de AWS para desplegar los recursos."
+  type        = string
+}
+
+variable "project_name" {
+  description = "Nombre del proyecto para prefijos de recursos."
+  type        = string
+}
+
+variable "account_number" {
+  description = "Numero de la cuenta AWS."
+  type        = string
+}
+
+variable "code_bucket" {
+  description = "Nombre del bucket para recursos de código."
+  type        = string
+}
+variable "lambda_name" {
+  description = "Nombre de la función Lambda."
+  type        = string
+}
+variable "lambda_arn" {
+  description = "Arn de la función Lambda."
+  type        = string
+}
+variable "cognito_user_pool_id" {
+  description = "ID del User Pool de Cognito."
+  type        = string
+}
