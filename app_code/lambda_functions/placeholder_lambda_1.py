52e50ce4df86c21de6fdfab99a7131a98b20642a
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 14:17:54 -0500

    ADD: Lambda placeholder code

---
+++ app_code/lambda_functions/placeholder_lambda_1.py
@@ -0,0 +1,8 @@
+def lambda_handler(event, context):
+    """
+    A simple AWS Lambda function placeholder.
+    """
+    return {
+        'statusCode': 200,
+        'body': 'Hello from Lambda!'
+    }
