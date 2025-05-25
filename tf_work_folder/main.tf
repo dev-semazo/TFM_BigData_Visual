64462b0ff57904ff732effbb017e4c576932704a
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 08:13:20 -0500

    ADD: Agregar providers y version de TF

--- tf_work_folder/main.tf
+++ tf_work_folder/main.tf
@@ -1,7 +1,14 @@
 terraform {
-  backend "s3" {
-    bucket = ""
-    key    = ""
-    region = ""
-  }
+    required_version = "1.2.5"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = ">= 2.7.0"
+        }
+    }
+    backend "s3" {
+        bucket = ""
+        key    = ""
+        region = ""
+    }
 }
