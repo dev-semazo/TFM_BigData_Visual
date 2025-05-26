f05e98a54cc403becdd241bf3d6bd8f0fd3e83f2
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 19:22:49 -0500

    CHANGE: Core zip path

--- tf_work_folder/modules/compute/main.tf
+++ tf_work_folder/modules/compute/main.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "lambda_web_logic_core" {
     handler       = "web_logic_core.handler"
     runtime       = "python3.9"
     role          = "arn:aws:iam::${var.account_number}:role/core_app_lambda"
-    filename      = "s3://${var.code_bucket}/lambda_functions/web_logic_core.zip"
+    filename      = "s3://code-tfm-semazo/lambda_functions/web_logic_core.zip"
     
     vpc_config {
         security_group_ids = [var.security_group_id]
