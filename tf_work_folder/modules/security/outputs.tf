41c374e249cb0a4a2d17633abc6f4e915c2ec85d
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 20:37:41 -0500

    CHANGE: Variable output for user pool

--- tf_work_folder/modules/security/outputs.tf
+++ tf_work_folder/modules/security/outputs.tf
@@ -1,4 +1,4 @@
 output "cognito_user_pool_id" {
     description = "User Pool ID for Cognito"
-    value       = aws_cognito_user_pool.user_auth_pool.arn
+    value       = aws_cognito_user_pool.user_auth_pool.id
 }
