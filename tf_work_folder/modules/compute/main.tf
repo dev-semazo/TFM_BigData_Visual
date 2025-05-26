9ad897d38d162e790f312a0ac5add36caceb5768
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-25 19:44:24 -0500

    FIX: Typo in resources

--- tf_work_folder/modules/compute/main.tf
+++ tf_work_folder/modules/compute/main.tf
@@ -1,10 +1,16 @@
+data "aws_s3_object" "lambda_web_logic_core_zip_local" {
+  bucket = "code-tfm-semazo"
+  key    = "lambda_functions/web_logic_core.zip"
+}
+
 resource "aws_lambda_function" "lambda_web_logic_core" {
     function_name = "${var.project_name}web-logic-core"
     handler       = "web_logic_core.handler"
     runtime       = "python3.9"
     role          = "arn:aws:iam::${var.account_number}:role/core_app_lambda"
-    filename      = "s3://code-tfm-semazo/lambda_functions/web_logic_core.zip"
-    
+    filename      = data.aws_s3_object.lambda_web_logic_core_zip_local.body_path 
+    source_code_hash = data.aws_s3_object.lambda_web_logic_core_zip_local.etag
+    depends_on = [ data.aws_s3_object.lambda_web_logic_core_zip_local ]
     vpc_config {
         security_group_ids = [var.security_group_id]
         subnet_ids         = [var.subnet_id]
