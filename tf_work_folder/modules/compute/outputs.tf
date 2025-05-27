01c2bab8d318ee2d8cbea95ca0f75b32686da7a8
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-05-26 22:07:44 -0500

    FIX: Delete ALB outputs

--- tf_work_folder/modules/compute/outputs.tf
+++ tf_work_folder/modules/compute/outputs.tf
@@ -7,12 +7,3 @@ output "lambda_arn" {
     description = "The ARN of the Lambda function"
     value       = aws_lambda_function.lambda_web_logic_core.arn
 }
-
-output "load_balancer_arn" {
-    description = "The ARN of the Network Load Balancer"
-    value       = aws_lb_listener.http_listener.arn
-}
-output "lambda_invoke_arn" {
-    description = "The ARN for invoking the Lambda function from API Gateway"
-    value       = aws_lambda_function.lambda_web_logic_core.invoke_arn
-}
