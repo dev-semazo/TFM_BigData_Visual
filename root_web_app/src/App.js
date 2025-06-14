4c0793b5c15a37e40b137afcc9d567460c09d1c4
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-14 01:47:16 -0500

    ADD: Dashboard and header

--- root_web_app/src/App.js
+++ root_web_app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import Dashboard from './dashboard';
+import Header from './header';
 import {Amplify} from 'aws-amplify';
 import awsconfig from './aws-exports';
 import { Authenticator, withAuthenticator } from '@aws-amplify/ui-react';
@@ -12,21 +14,11 @@ function App() {
     <div className="App">
       <Authenticator>
         {({ signOut }) => (
-        <header className="App-header">
-          <h2>Testing App</h2>
-          <button 
-                onClick={signOut} 
-                style={{ 
-                  margin: '20px', 
-                  fontSize: '0.8rem', 
-                  padding: '5px 10px', 
-                  marginTop: '20px'
-                }}
-              >
-            Cerrar Sesión
-          </button>
-          
-        </header>
+        <main className="App-main">
+          <Header />
+          <Dashboard />
+          <button onClick={signOut} id="logoutbutton">Cerrar Sesión</button>
+        </main>
         )}
       </Authenticator>
     </div>
---
+++ root_web_app/src/header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <header style={{ 
+            backgroundColor: '#282c34', 
+            padding: '20px', 
+            color: 'white', 
+            textAlign: 'center' 
+        }}>
+            <h1>Educ App SD</h1>
+        </header>
+    );
+}
+
+export default Header;
