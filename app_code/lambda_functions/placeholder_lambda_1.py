66017500798d2a9a6514874bad7c6a2ddbb20861
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-14 00:23:54 -0500

    CHANGE: App path

--- app_code/lambda_functions/placeholder_lambda_1.py
+++
@@ -1,8 +0,0 @@
-def lambda_handler(event, context):
-    """
-    A simple AWS Lambda function placeholder.
-    """
-    return {
-        'statusCode': 200,
-        'body': 'Hello from Lambda!'
-    }
