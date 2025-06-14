c774974268a49ca63e86ccaf30d5cdbca9362092
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-14 15:04:36 -0500

    CHANGE: API Response

--- app_code/lambda_functions/web_logic_core.py
+++ app_code/lambda_functions/web_logic_core.py
@@ -1,8 +1,17 @@
+import json
+
 def lambda_handler(event, context):
     """
     A simple AWS Lambda function placeholder.
     """
     return {
         'statusCode': 200,
-        'dashboard': 'Hello from Lambda!'
+        'headers': {
+            "Access-Control-Allow-Origin": "https://web.d347kktgec41m0.amplifyapp.com",
+            "Access-Control-Allow-Headers": "Content-Type,authorization",
+            "Access-Control-Allow-Methods": "GET"
+        },
+        'body': json.dumps({ 
+            'dashboard': "placeholder"
+        })
     }
