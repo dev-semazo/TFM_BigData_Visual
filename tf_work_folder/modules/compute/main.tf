1fbdc0bf4d68ca3e3d23d05730daebf976712d7e
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-19 18:28:44 -0500

    ADD: Bronce etl lambda

--- tf_work_folder/modules/compute/main.tf
+++ tf_work_folder/modules/compute/main.tf
@@ -10,3 +10,12 @@ resource "aws_lambda_function" "lambda_web_logic_core" {
     
 }
 
+resource "aws_lambda_function" "bronce_etl_logic" {
+    function_name = "${var.project_name}etl_bronce"
+    handler       = "etl_bronce.lambda_handler"
+    runtime       = "python3.9"
+    role          = "arn:aws:iam::${var.account_number}:role/core_app_lambda"
+    s3_bucket     = var.code_bucket
+    s3_key        = "lambda_functions/etl_bronce.zip"
+}
+
