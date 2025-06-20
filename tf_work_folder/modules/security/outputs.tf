c6025f54bdb4d520c734540223a1e99c09d57fed
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-19 19:14:02 -0500

    DELETE: Old Cognito User Pool

--- tf_work_folder/modules/security/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "cognito_user_pool_id" {
-    description = "User Pool ID for Cognito"
-    value       = aws_cognito_user_pool.user_auth_pool.id
-}
--- tf_work_folder/modules/security/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "aws_region" {
-  description = "Región de AWS para desplegar los recursos."
-  type        = string
-}
-
-variable "project_name" {
-  description = "Nombre del proyecto para prefijos de recursos."
-  type        = string
-}
