2e6a1c1154ef7000ea46e89d2769932fb1fae79a
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-19 19:07:05 -0500

    ADD: Zip Logic for workflow

---
+++ .github/workflows/zip_files.py
@@ -0,0 +1,9 @@
+import zipfile, os
+
+FOLDER_TO_ZIP = 'app_code/lambda_functions/'
+
+for file in os.listdir(FOLDER_TO_ZIP):
+    print(file)
+    if file.endswith('.py'):
+        with zipfile.ZipFile(FOLDER_TO_ZIP +'/'+file.split('.')[0]+".zip",'w') as zip_file:
+            zip_file.write(FOLDER_TO_ZIP + file)
---
+++ app_code/lambda_functions/etl_bronce.py
@@ -0,0 +1,16 @@
+def lambda_handler(event, context):
+    """
+    Placeholder Lambda function for ETL Bronze layer.
+
+    Args:
+        event (dict): Input event data.
+        context (LambdaContext): Runtime information.
+
+    Returns:
+        dict: Response with status.
+    """
+    # TODO: Implement ETL logic for Bronze layer
+    return {
+        'statusCode': 200,
+        'body': 'ETL Bronze Lambda placeholder executed successfully.'
+    }
