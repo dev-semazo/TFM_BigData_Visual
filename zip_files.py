d77113c362e16bf7b0d9dd5286522aa370133532
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-19 19:24:49 -0500

    CHANGE: Sync only zip files

--- zip_files.py
+++ zip_files.py
@@ -1,10 +1,10 @@
 import zipfile, os
 
 FOLDER_TO_ZIP = 'app_code/lambda_functions/'
-
-for file in os.listdir(FOLDER_TO_ZIP):
-    print(file)
+files_to_zip = os.listdir(FOLDER_TO_ZIP)
+os.chdir(FOLDER_TO_ZIP)
+for file in files_to_zip:
     if file.endswith('.py'):
-        with zipfile.ZipFile(FOLDER_TO_ZIP +'/'+file.split('.')[0]+".zip",'w') as zip_file:
-            zip_file.write(FOLDER_TO_ZIP + file)
+        with zipfile.ZipFile(file.split('.')[0]+".zip",'w') as zip_file:
+            zip_file.write(file)
 
