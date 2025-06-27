331d8fd70b7451cafa5860aa7f26ba1bdfd01790
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-26 19:21:13 -0500

    FIX: Layer arn

--- tf_work_folder/modules/compute/main.tf
+++ tf_work_folder/modules/compute/main.tf
@@ -8,9 +8,6 @@ resource "aws_lambda_function" "bronce_etl_logic" {
     s3_key        = "etl_bronce.zip"
     memory_size   = 256
     timeout       = 600
-    id            = "tfm-educ-app-bronce_silver"
-    layers        = [
-          - "arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:29",
-    ]
+    layers        = ["arn:aws:lambda:us-east-1:336392948345:layer:AWSSDKPandas-Python39:29"]
 }
 
