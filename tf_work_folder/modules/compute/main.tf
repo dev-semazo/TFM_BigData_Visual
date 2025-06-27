f6da84510145d185b763f22d9b73fb73f88bb291
Sebastián Mazo Vélez <sebastianmazovelez@gmail.com>
2025-06-26 19:16:35 -0500

    ADD: Create Crawlers for GLue Tables

--- tf_work_folder/modules/compute/main.tf
+++ tf_work_folder/modules/compute/main.tf
@@ -6,5 +6,7 @@ resource "aws_lambda_function" "bronce_etl_logic" {
     role          = "arn:aws:iam::${var.account_number}:role/core_app_lambda"
     s3_bucket     = var.code_bucket
     s3_key        = "etl_bronce.zip"
+    memory_size   = 256
+    timeout       = 600
 }
 
--- tf_work_folder/modules/data_lake/main.tf
+++ tf_work_folder/modules/data_lake/main.tf
@@ -54,10 +54,6 @@ resource "aws_s3_bucket_server_side_encryption_configuration" "gold_encryption" 
 #----------------------------------
 # AWS Glue Data Catalog
 #---------------------------------
-resource "aws_glue_catalog_database" "bronze_db" {
-  name = "${var.project_name}bronze"
-  description = "Base de datos bronze del Data Lake"
-}
 
 resource "aws_glue_catalog_database" "silver_db" {
   name = "${var.project_name}silver"
@@ -83,5 +79,90 @@ resource "aws_athena_workgroup" "quicksight_workgroup" {
       output_location = "s3://${var.project_name}bronze/athena_results/"
     }
     publish_cloudwatch_metrics_enabled = false
+  }
+}
+
+#----------------------------------
+# IAM Role
+#---------------------------------
+resource "aws_iam_role" "crawl_role" {
+  name = "glue_crawler_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "glue.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "tag-value"
+  }
+}
+
+resource "aws_iam_role_policy" "crawl_policy" {
+  name = "crawler_policy"
+  role = aws_iam_role.crawl_role.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+              "s3:GetObject"
+          ],
+          "Resource": [
+            "arn:aws:s3:::tfm-educ-app-gold",
+            "arn:aws:s3:::tfm-educ-app-gold/*",
+            "arn:aws:s3:::tfm-educ-app-silver",
+            "arn:aws:s3:::tfm-educ-app-silver/*"
+          ]
+        }
+    ]
+  })
+}
+
+#----------------------------------
+# Glue Crawler
+#---------------------------------
+
+resource "aws_glue_crawler" "crawler_silver_munic" {
+  database_name = aws_glue_catalog_database.silver_db.name
+  name          = "gc_silver_munic"
+  role          = aws_iam_role.rawl_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.silver_bucket.bucket}/cobertura_municipios/"
+  }
+}
+
+
+resource "aws_glue_crawler" "crawler_silver_nived" {
+  database_name = aws_glue_catalog_database.silver_db.name
+  name          = "gc_silver_nived"
+  role          = aws_iam_role.rawl_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.silver_bucket.bucket}/nivel_educativo_edad/"
+  }
+}
+
+
+resource "aws_glue_crawler" "crawler_silver_mat_educ" {
+  database_name = aws_glue_catalog_database.silver_db.name
+  name          = "gc_silver_mat_educ"
+  role          = aws_iam_role.rawl_role.arn
+
+  s3_target {
+    path = "s3://${aws_s3_bucket.silver_bucket.bucket}/matriculas_educacion/"
   }
 }
